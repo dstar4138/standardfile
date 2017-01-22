2208f73ce07cc5361c4790adedd029325100d638
Alexander Dean <dstar@slackless.com>
2017-01-22 03:26:45 -0500

    Add SSL support.

--- src/env.rs
+++ src/env.rs
@@ -7,6 +7,7 @@ pub const LOCALHOST: &'static str = "0.0.0.0";
 /* Default Parameters. */
 pub const DEFAULT_PORT: &'static str = "8080";
 
+//TODO:Allow to pull from ENV or file so pass isn't in args.
 pub fn build_arg_parser<'a,'b>() -> App<'a,'b> {
     App::new(APP_NAME)
         .version(crate_version!())
@@ -16,5 +17,24 @@ pub fn build_arg_parser<'a,'b>() -> App<'a,'b> {
                 .long("port")
                 .value_name("PORT")
                 .default_value(DEFAULT_PORT))
+
+        .arg(
+            // Toggle on SSL.
+            Arg::with_name("ssl")
+                .long("ssl")
+                .index(1)
+                .requires_all(&["TLSIdentity","TLSPassword"]))
+        .arg(
+            Arg::with_name("TLSIdentity")
+                .requires("ssl")
+                .short("I")
+                .long("TLSIdentityFile")
+                .value_name("FILE"))
+        .arg(
+            Arg::with_name("TLSPassword")
+                .requires("ssl")
+                .short("P")
+                .long("TLSPassword")
+                .value_name("PASS"))
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 extern crate hyper;
+extern crate hyper_native_tls;
 extern crate standardfile;
 
 use standardfile::env;
 use standardfile::service;
 //use hyper::server::Http;
 use hyper::server::Server;
+use hyper_native_tls::NativeTlsServer;
 
+// To test https, create a local self-signed cert:
+// openssl req -x509 -newkey rsa:4096 -nodes -keyout localhost.key -out localhost.crt -days 3650
+// openssl pkcs12 -export -out identity.p12 -inkey localhost.key -in localhost.crt --password mypass
 fn main() {
     let args = env::build_arg_parser().get_matches();
     let port = args.value_of("port").unwrap();
@@ -19,6 +24,14 @@ fn main() {
     server.run().unwrap();
     */
     println!("listening on {}",addr);
-    let server = Server::http(addr.as_str()).unwrap();
-    let _guard = server.handle(service::handle);
+    if args.is_present("ssl") {
+        let identity = args.value_of("TLSIdentity").unwrap();
+        let pass = args.value_of("TLSPassword").unwrap();
+        let ssl = NativeTlsServer::new(identity, pass).unwrap();
+        let server = Server::https(addr.as_str(), ssl).unwrap();
+        let _guard = server.handle(service::handle);
+    } else {
+        let server = Server::http(addr.as_str()).unwrap();
+        let _guard = server.handle(service::handle);
+    }
 }
