0090ea7ef43b8fe3bad8ffc9c614bf0012d1682f
Alexander Dean <dstar@slackless.com>
2017-01-23 01:53:29 -0500

    Add echo api for shallow ping tests.

--- src/service.rs
+++ src/service.rs
@@ -6,8 +6,9 @@ static INDEX: &'static [u8] = b"For API, see https://standardfile.org";
 
 pub fn handler() -> Router {
     return router!(
+        index:      any   "/"             => index,
+        echo:       get   "/echo/:q"      => echo /*,
         // AUTH -------------------------------------
-        index:      any   "/"             => index /*,
         auth:       post  "/auth"         => auth,
         up_auth:    patch "/auth"         => up_auth,
         sign_in:    post  "/auth/sign_in" => sign_in,
@@ -24,3 +25,8 @@ fn index(_: &mut Request) -> IronResult<Response> {
     )
 }
 
+fn echo(req: &mut Request) -> IronResult<Response> {
+    let ref query = req.extensions.get::<Router>()
+            .unwrap().find("q").unwrap_or("/");
+    Ok(Response::with((status::Ok, *query)))
+}
