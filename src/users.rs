211e95a04f99ef34d5f09691e98a84991492073b
Alexander Dean <dstar@slackless.com>
2017-02-10 02:31:14 -0500

    Break out Password Details into encodable object.

--- src/users.rs
+++ src/users.rs
@@ -6,15 +6,20 @@ pub struct SFUser {
     pub uuid: Uuid,
 
     pub encrypted_password: String,
+    pub pwd: PWDetails,
+    pub email: String,
+    pub created_at: DateTime<UTC>,
+    pub updated_at: DateTime<UTC>,
+}
+#[derive(Serialize,Deserialize,Debug,PartialEq,Eq)]
+pub struct PWDetails {
     pub pw_func: String,
     pub pw_alg: String,
     pub pw_cost: i32,
     pub pw_key_size: i32,
     pub pw_nonce: String,
-    pub email: String,
-    pub created_at: DateTime<UTC>,
-    pub updated_at: DateTime<UTC>,
 }
+
 
 pub fn create_new(email: String,
                   password: String,
@@ -30,11 +35,13 @@ pub fn create_new(email: String,
         // ruby-server uses SecureRandom.uuid
         uuid: Uuid::new_v4(),
         encrypted_password: password,
-        pw_func: pw_func,
-        pw_alg: pw_alg,
-        pw_cost: pw_cost,
-        pw_key_size: pw_key_size,
-        pw_nonce: pw_nonce,
+        pwd: PWDetails {
+            pw_func: pw_func,
+            pw_alg: pw_alg,
+            pw_cost: pw_cost,
+            pw_key_size: pw_key_size,
+            pw_nonce: pw_nonce,
+        },
         email: email,
         created_at: cur_time,
         updated_at: cur_time,
@@ -49,15 +56,21 @@ pub fn update(newpass: String, user: &SFUser) -> SFUser {
 
     SFUser {
         uuid: user.uuid.clone(),
-        pw_func: user.pw_func.clone(),
-        pw_alg: user.pw_alg.clone(),
-        pw_cost: user.pw_cost.clone(),
-        pw_key_size: user.pw_key_size.clone(),
-        pw_nonce: user.pw_nonce.clone(),
+        pwd: get_pw_details(&user),
         email: user.email.clone(),
         created_at: user.created_at.clone(),
 
         encrypted_password: newpass,
         updated_at: cur_time,
+    }
+}
+
+pub fn get_pw_details(user: &SFUser) -> PWDetails {
+    PWDetails {
+        pw_func: user.pwd.pw_func.clone(),
+        pw_alg: user.pwd.pw_alg.clone(),
+        pw_cost: user.pwd.pw_cost.clone(),
+        pw_key_size: user.pwd.pw_key_size.clone(),
+        pw_nonce: user.pwd.pw_nonce.clone(),
     }
 }
